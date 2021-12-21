e9752baf09b989fba4b42141e107500683efaae3
Jayklef <arhawhojerry@gmail.com>
2021-12-21 12:15:46 +0100

    Initial creation of  project with packages setup

---
+++ src/main/java/com/jayklef/mapxenses/MapxensesApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.mapxenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapxensesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapxensesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayklef/mapxenses/MapxensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayklef.mapxenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapxensesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
