24e0b9b1683c49b1bf7b85caf702eab399b171ec
Jayklef <jerryarhawho@gmail.com>
2022-01-31 13:24:39 +0100

    Added RestTemplate to project for consuming charts

--- src/main/java/com/jayklef/mapxenses/Expense/utils/Utility.java
+++ src/main/java/com/jayklef/mapxenses/Expense/utils/Utility.java
@@ -1,9 +1,11 @@
 package com.jayklef.mapxenses.Expense.utils;
 
+import com.jayklef.mapxenses.Expense.model.Expense;
 import com.jayklef.mapxenses.Expense.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.util.Collections;
@@ -15,6 +17,9 @@ public class Utility {
     @Autowired
     private ExpenseService expenseService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public ResponseEntity<Double> calculateWeeklyExpenses(LocalDate startDate, LocalDate endDate) {
         Double expenses = expenseService.calculateWeeklyExpenses(startDate, endDate);
         return new ResponseEntity<>(expenses, HttpStatus.OK);
@@ -23,5 +28,10 @@ public class Utility {
     public ResponseEntity<Double> monthlyExpenses(LocalDate startDate, LocalDate endDate){
         List<Double> monthlyExpenses = Collections.singletonList(expenseService.calculateMonthlyExpenses(startDate, endDate));
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public Object ExpenseChart(){
+        Expense barchart = restTemplate.getForObject("https://code.highcharts.com/highcharts.js",Expense.class);
+        return barchart.getClass();
     }
 }
