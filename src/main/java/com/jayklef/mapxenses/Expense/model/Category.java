bedc873eb57daf0942dd808f6bd40635e8897193
Jayklef <jerryarhawho@gmail.com>
2022-08-01 19:06:15 +0100

    mapped category to expenses

--- src/main/java/com/jayklef/mapxenses/Expense/model/Category.java
+++ src/main/java/com/jayklef/mapxenses/Expense/model/Category.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,5 +16,8 @@ public class Category {
     @Id
     private Long id;
     private String name;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    private List<Expense> expenses;
 
 }
